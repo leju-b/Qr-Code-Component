42cd17a3577a20561105a6ea04854bf233407bee
leju b <iamleju02@gmail.com>
2023-07-27 23:09:26 +0530

    commit-begin

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <section>
+      <div className="main">
+        <img src="./images/image-qr-code.png" alt="" />
+        <div className="desc">
+          <h3>Improve your front-end skills by building projects.</h3>
+          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div> 
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default App

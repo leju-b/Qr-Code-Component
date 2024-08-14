b38207dd37cfe673c179c7731483684d120cefbd
Leju B <iamleju02@gmail.com>
2024-08-14 15:49:15 +0530

    added new qr

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
     <>
     <section>
       <div className="main">
-        <img src="./images/image-qr-code.png" alt="" />
+        <img src="./images/frame.png" alt="" />
         <div className="desc">
-          <h3>Improve your front-end skills by building projects.</h3>
-          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+          <h3>Hire me? Take a look at my skill set.</h3>
+          <p>Scan the QR code to visit my personal portfolio</p>
         </div> 
       </div>
     </section>
